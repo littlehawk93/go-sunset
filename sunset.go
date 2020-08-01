b121c5da00c277ff4917a0f8877a422f566344f2
John Hawkins <littlehawk93@users.noreply.github.com>
2020-08-01 01:36:39 -0500

    initial code commit

---
+++ sunset.go
@@ -0,0 +1,70 @@
+package gosunset
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+const baseURL string = "https://api.sunrise-sunset.org/json"
+
+// Result an API result returned from the sunrise-sunset.org API
+type Result struct {
+	Sunrise                   time.Time `json:"sunrise"`
+	Sunset                    time.Time `json:"sunset"`
+	DayLength                 int       `json:"day_length"`
+	SolarNoon                 time.Time `json:"solar_noon"`
+	CivilTwilightBegin        time.Time `json:"civil_twilight_begin"`
+	CivilTwilightEnd          time.Time `json:"civil_twilight_end"`
+	NauticalTwilightBegin     time.Time `json:"nautical_twilight_begin"`
+	NauticalTwilightEnd       time.Time `json:"nautical_twilight_end"`
+	AstronomicalTwilightBegin time.Time `json:"astronomical_twilight_begin"`
+	AstronomicalTwilightEnd   time.Time `json:"astronomical_twilight_end"`
+}
+
+type results struct {
+	Results []Result `json:"results"`
+	Status  string   `json:"status"`
+}
+
+// GetResult retrieve a new result from sunrise-sunset.org API. Returns the API result or any errors encountered.
+func GetResult(lat, lng float32) (*Result, error) {
+
+	u, _ := url.Parse(baseURL)
+
+	q := u.Query()
+	q.Add("lat", fmt.Sprintf("%10.7f", lat))
+	q.Add("lng", fmt.Sprintf("%10.7f", lng))
+	q.Add("formatted", fmt.Sprintf("%d", 0))
+	u.RawQuery = q.Encode()
+
+	resp, err := http.Get(u.String())
+
+	if err != nil {
+		return nil, fmt.Errorf("Error retrieving API: %s", err.Error())
+	}
+
+	if resp.Body == nil {
+		return nil, fmt.Errorf("Response body is nil")
+	}
+
+	defer resp.Body.Close()
+
+	var apiResults results
+
+	if err := json.NewDecoder(resp.Body).Decode(&apiResults); err != nil {
+		return nil, fmt.Errorf("Unable to parse API response: %s", err.Error())
+	}
+
+	if apiResults.Status != "OK" {
+		return nil, fmt.Errorf("Error in API response. Status: %s", apiResults.Status)
+	}
+
+	if len(apiResults.Results) == 0 {
+		return nil, fmt.Errorf("No results returned by API")
+	}
+
+	return &apiResults.Results[0], nil
+}
